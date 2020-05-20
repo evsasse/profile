ad3f7e621611294038099ecbf2f361e840aa9e81
evsasse <evsasse@gmail.com>
2020-05-20 14:02:27 -0300

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a35d5a720091375ddf56";
+/******/ 	var hotCurrentHash = "0873800fa92360ab0233";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
